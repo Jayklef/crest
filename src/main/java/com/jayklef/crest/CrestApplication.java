b6f059290b484f61ed8d5dd1be118af1a277b2c8
Jayklef <jerryarhawho@gmail.com>
2023-05-22 18:34:58 +0100

    Initial project setup

---
+++ src/main/java/com/jayklef/crest/CrestApplication.java
@@ -0,0 +1,13 @@
+package com.jayklef.crest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrestApplication.class, args);
+    }
+
+}
