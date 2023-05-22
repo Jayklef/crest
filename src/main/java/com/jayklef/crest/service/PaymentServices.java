6abebfa1b5c521e6d7599f52c80cda95fc7cf067
Jayklef <jerryarhawho@gmail.com>
2023-05-22 23:59:08 +0100

    Client secret, clientID and sandbox

---
+++ src/main/java/com/jayklef/crest/service/PaymentServices.java
@@ -0,0 +1,8 @@
+package com.jayklef.crest.service;
+
+public class PaymentServices {
+
+    private static final String CLIENT_ID = "AbBvXQvD-6GKiOP91M1GksU2z0YGCpLpXCWIJyphwfx9iO95l2TghTVzKy3cTmJbuQ4cm2JQp1Ooa6Ov";
+    private static final String CLIENT_SECRET = "EE1f2qkQ82yz6p8JGw3quMkBFozU4pWZqrX0uj5ukDogHddPtBEtO5xgnTZ-axwcNClT-Lv2ARr3G7rJ";
+    private static final String MODE = "sandbox";
+}
