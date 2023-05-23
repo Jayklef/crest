45fcff5c460b518ff104d4292edccdb6a4fba572
Jayklef <jerryarhawho@gmail.com>
2023-05-23 09:00:08 +0100

    Approval Links, Amount, Transaction

--- src/main/java/com/jayklef/crest/model/OrderDetail.java
+++ src/main/java/com/jayklef/crest/model/OrderDetail.java
@@ -3,15 +3,15 @@ package com.jayklef.crest.model;
 public class OrderDetail {
 
     private String productName;
-    private float subtotal;
     private float shipping;
+    private float subtotal;
     private float tax;
     private float total;
 
-    public OrderDetail(String productName, float subtotal, float shipping, float tax, float total) {
+    public OrderDetail(String productName, float shipping, float subtotal, float tax, float total) {
         this.productName = productName;
-        this.subtotal = subtotal;
         this.shipping = shipping;
+        this.subtotal = subtotal;
         this.tax = tax;
         this.total = total;
     }
@@ -24,32 +24,32 @@ public class OrderDetail {
         this.productName = productName;
     }
 
-    public float getSubtotal() {
-        return subtotal;
+    public String getSubtotal() {
+        return String.format("%.2f", subtotal);
     }
 
-    public void setSubtotal(float subtotal) {
-        this.subtotal = subtotal;
-    }
-
-    public float getShipping() {
-        return shipping;
+    public String getShipping() {
+        return String.format("%.2f", shipping);
     }
 
     public void setShipping(float shipping) {
         this.shipping = shipping;
     }
 
-    public float getTax() {
-        return tax;
+    public void setSubtotal(float subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public String getTax() {
+        return String.format("%.2f", tax);
     }
 
     public void setTax(float tax) {
         this.tax = tax;
     }
 
-    public float getTotal() {
-        return total;
+    public String getTotal() {
+        return String.format("%.2f", total);
     }
 
     public void setTotal(float total) {
--- src/main/java/com/jayklef/crest/service/PaymentServices.java
+++ src/main/java/com/jayklef/crest/service/PaymentServices.java
@@ -1,9 +1,12 @@
 package com.jayklef.crest.service;
 
 import com.jayklef.crest.model.OrderDetail;
-import com.paypal.api.payments.Payer;
-import com.paypal.api.payments.PayerInfo;
-import com.paypal.api.payments.RedirectUrls;
+import com.paypal.api.payments.*;
+import com.paypal.base.rest.APIContext;
+import com.paypal.base.rest.PayPalRESTException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PaymentServices {
 
@@ -11,11 +14,36 @@ public class PaymentServices {
     private static final String CLIENT_SECRET = "EE1f2qkQ82yz6p8JGw3quMkBFozU4pWZqrX0uj5ukDogHddPtBEtO5xgnTZ-axwcNClT-Lv2ARr3G7rJ";
     private static final String MODE = "sandbox";
 
-    public String authorizePayment(OrderDetail orderDetail){
+    public String authorizePayment(OrderDetail orderDetail) throws PayPalRESTException {
         Payer payer = getPayerInformation();
-
         RedirectUrls redirectUrls = getRedirectUrls();
-        return null;
+        List<Transaction> transactions = getTransactionInformation(orderDetail);
+
+        Payment requestPayment = new Payment();
+        requestPayment.setTransactions(transactions)
+                      .setRedirectUrls(redirectUrls)
+                      .setPayer(payer)
+                      .setIntent("authorize");
+
+        APIContext apiContext = new APIContext(CLIENT_ID, CLIENT_SECRET, MODE);
+        Payment approvedPayment = requestPayment.create(apiContext);
+
+        System.out.println(approvedPayment);
+
+        return getApprovalLinks(approvedPayment);
+    }
+
+    private String getApprovalLinks(Payment approvedPayment){
+        List<Links> links = approvedPayment.getLinks();
+        String approvalLink = null;
+
+        for (Links link : links){
+            if (link.getRel().equalsIgnoreCase("approval_Url")){
+                approvalLink = link.getHref();
+            }
+        }
+
+        return approvalLink;
     }
 
     private RedirectUrls getRedirectUrls(){
@@ -24,6 +52,41 @@ public class PaymentServices {
         redirectUrls.setReturnUrl("http://localhost:63342/crest/templates/review_payment");
 
         return redirectUrls;
+    }
+
+    private List<Transaction> getTransactionInformation(OrderDetail orderDetail){
+        Details details = new Details();
+        details.setShipping(orderDetail.getShipping());
+        details.setSubtotal(orderDetail.getSubtotal());
+        details.setTax(orderDetail.getTax());
+
+        Amount amount = new Amount();
+        amount.setCurrency("NGN");
+        amount.setTotal(orderDetail.getTotal());
+        amount.setDetails(details);
+
+        Transaction transaction = new Transaction();
+        transaction.setAmount(amount);
+        transaction.setDescription(orderDetail.getProductName());
+
+        ItemList itemList = new ItemList();
+        List<Item> items = new ArrayList<>();
+
+        Item item = new Item();
+        item.setCurrency("NGN");
+        item.setName(orderDetail.getProductName());
+        item.setPrice(orderDetail.getTotal());
+        item.setTax(orderDetail.getTax());
+        item.setQuantity("");
+
+        items.add(item);
+        itemList.setItems(items);
+        transaction.setItemList(itemList);
+
+        List<Transaction> transactions = new ArrayList<>();
+        transactions.add(transaction);
+
+        return transactions;
     }
 
     private Payer getPayerInformation() {
